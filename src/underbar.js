7f0e73bf7e1f1b6bb842f7b4b884adb714bb30c9
Connie Cheung <conniedcheung@gmail.com>
2016-01-25 00:49:12 -0800

    extend: 1st pass

--- src/underbar.js
+++ src/underbar.js
@@ -227,6 +227,11 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i=1; i<arguments.length; i++) {
+      for (var k in i) {
+        obj[k] = i[k];
+      }
+    }
   };
 
   // Like extend, but doesn't ever overwrite a key that already
