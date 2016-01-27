f49f09f4ed30dea58328c43de539b8bc57db8cf1
Connie Cheung <conniedcheung@gmail.com>
2016-01-27 13:48:30 -0800

    extend

--- src/underbar.js
+++ src/underbar.js
@@ -255,6 +255,16 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var newObjs=Array.prototype.slice.call(arguments, 1);
+
+    _.each(newObjs, function(otherObj) {
+      _.each(otherObj, function(val, key) {
+        obj[key]=val;
+      });
+    });
+
+    return obj;
+
   };
 
   // Like extend, but doesn't ever overwrite a key that already
