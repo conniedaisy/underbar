b07cd8a2d07753812c27d8b3f7d8b372ddc23273
Connie Cheung <conniedcheung@gmail.com>
2016-01-28 15:02:00 -0800

    checking contains

--- src/underbar.js
+++ src/underbar.js
@@ -190,12 +190,24 @@
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
+    /*
     return _.reduce(collection, function(wasFound, item) {
       if (wasFound) {
         return true;
       }
       return item === target;
     }, false);
+    */
+
+    //for each item in collection, check if equals to (===) target
+    //if yes, return
+    for (var i = 0; i < collection.length; i++) {
+      if (collection[i] === target) {
+        return true;
+      }
+      return false;
+    }
+
   };
 
 //FINISHED
